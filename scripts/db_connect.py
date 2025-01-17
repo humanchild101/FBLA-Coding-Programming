779bff02f8d4636f5e5b5bfe81661b4757707380
Sharvikaks <sharvika.ks@gmail.com>
2025-01-16 22:21:58 -0500

    Successfully connect db

--- scripts/db_connect.py
+++ scripts/db_connect.py
@@ -2,9 +2,40 @@
 import logging
 import psycopg2 # type: ignore
 
-#db_connect is a class that is used to connect the database to the program and execute query.
-class db_connect:
-    #get_connection is a method that is used to get the db connection.
-    def get_connection(self, connection_string):
-        psycopg2.connect()
-        return "Connection object"
+
+# Connection parameters to the database
+db_params = {
+    "host": "localhost",
+    "database": "postgres",
+    "user": "postgres",
+    "password": "password123",
+    "port": 5432  
+}
+
+try:
+    # Connect to the database
+    conn = psycopg2.connect(**db_params)
+
+    # Create a cursor object
+    #Cursor is an object used to interact with the database by executing SQL queries
+    cursor = conn.cursor()
+
+    # Execute a query
+    cursor.execute("SELECT version();")
+
+    # Fetch the result
+    result = cursor.fetchone()
+
+    # Print the result
+    print(result)
+
+except (Exception, psycopg2.Error) as error:
+    print("Error connecting to database:", error)
+
+finally:
+    # Close the cursor and connection
+    if cursor:
+        cursor.close()
+    if conn:
+        conn.close()
+
