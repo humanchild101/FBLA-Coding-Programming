bb90f817965e2a9173c1038674f878deef8a5560
Nikhila Mannepalli <nikhilamadhavi@gmail.com>
2025-01-18 23:04:46 -0500

    putting stuff into seperate files and importing them will make it more neater i think

--- venv/login.py
+++ venv/login.py
@@ -1,6 +1,10 @@
 import customtkinter as ctk
 import tkinter as tk  # for StringVar + messagebox + lines
 from tkinter import messagebox
+import root_window
+from root_window import *
+
+root_window.root_init()
 
 # bg = #1B4965
 # border = #BEE9E8
@@ -8,11 +12,10 @@ from tkinter import messagebox
 # labels = #3095AE    (hover color = #246690)
 # entries = #BEE9E8
 
-root = ctk.CTk()
-root.title("FBLA Coding and Programming Sharvika & Nikhila")
-root.geometry('1000x1000')
-root.configure(fg_color = '#1B4965') #62B6CB
-root.resizable(True, True)
+#------------------------
+user_var = tk.StringVar(value="Username Here")  # 1B4965 and then BEE9E8
+password_var = tk.StringVar(value="Password Here")  # 1B4965 and then BEE9E8
+
 
 def on_login():
     username_in = user_var.get()
@@ -33,42 +36,48 @@ def forgot_password():
 
 def create_new_account():
     messagebox.showinfo("Message", "Will create a seperate page for input details to make a new account. somehow have to add this into the database")
-# center frame w/ border
-center_border = ctk.CTkFrame(root, fg_color = "#BEE9E8", corner_radius=20, width = 620, height= 355)
-center_border.place(relx= 0.5, rely = 0.5, anchor = "center")
-center_frame = ctk.CTkFrame(center_border, fg_color = '#62B6CB', corner_radius=15)  #62B6CB
-center_frame.place(relx = 0.5, rely = 0.5, anchor="center")
 
-# login label 1B4965
-loginL = ctk.CTkLabel(center_frame, text = "Login To Your Account: ",text_color="black", font = ("Arial", 20), fg_color = "#3095AE", padx = 10, pady = 10, corner_radius=15, width = 600)
-loginL.grid(row = 0, column = 0, columnspan = 2, padx = 5, pady = 5)
 
-# username entry
-user_var = tk.StringVar(value = "Username Here") #1B4965 and then BEE9E8
-username_label = ctk.CTkLabel(center_frame, text = "Username", font = ("Arial", 18), fg_color = "#3095AE", text_color = "black", padx = 5, pady = 6,corner_radius=5)
-username_input = ctk.CTkEntry(center_frame, textvariable=user_var, font=("Arial", 15), fg_color="#BEE9E8", text_color="black", width=400, corner_radius=5)
-username_label.grid(row=1, column=0, sticky="e", padx=5, pady=10, ipady = 4)
-username_input.grid(row=1, column=1, sticky="w", padx=20, pady=10, ipady=5)
+def show_login_page():
+    login_page = ctk.CTkFrame(root, fg_color="#1B4965")
+    login_page.pack(fill="both", expand=True)  # #62B6CB
 
-# password entry
-password_var = tk.StringVar(value="Password Here")#1B4965 and then BEE9E8
-password_label = ctk.CTkLabel(center_frame, text="Password ", font=("Arial", 18), fg_color="#3095AE",text_color="black", padx=5, pady=6, corner_radius=5)
-password_input = ctk.CTkEntry(center_frame, textvariable=password_var, font=("Arial", 15), fg_color="#BEE9E8", text_color = "black", width=400, show="*", corner_radius=5)
-password_label.grid(row=2, column=0, sticky="e", padx=5, pady=10,ipady = 4)
-password_input.grid(row=2, column=1, sticky="w", padx=20, pady=10, ipady=5)
+    # center frame w/ border
+    center_border = ctk.CTkFrame(login_page, fg_color = "#BEE9E8", corner_radius=20, width = 620, height= 355)
+    center_border.place(relx= 0.5, rely = 0.5, anchor = "center")
+    center_frame = ctk.CTkFrame(center_border, fg_color = '#62B6CB', corner_radius=15)  #62B6CB
+    center_frame.place(relx = 0.5, rely = 0.5, anchor="center")
 
-# forgot password button
-forgot_button = ctk.CTkButton(center_frame, text="Forgot Password?", font=("Arial", 13), fg_color="#3095AE", hover_color="#246690", text_color="black", width=130, corner_radius=5, command=forgot_password)
-forgot_button.grid(row=3, column=1, columnspan=2, padx=50, sticky="e")
+    # login label 1B4965
+    loginL = ctk.CTkLabel(center_frame, text = "Login To Your Account: ",text_color="black", font = ("Arial", 20), fg_color = "#3095AE", padx = 10, pady = 10, corner_radius=15, width = 600)
+    loginL.grid(row = 0, column = 0, columnspan = 2, padx = 5, pady = 5)
 
-# login button #1B4965 and then 246690
-login_button = ctk.CTkButton(center_frame, text="Login", font=("Arial", 18), fg_color="#3095AE", hover_color="#246690", text_color="black", width=200, corner_radius=5, command=on_login)
-login_button.grid(row=4, column=0, columnspan=2, pady=5, padx=50, ipady = 5)
+    # username entry
+    username_label = ctk.CTkLabel(center_frame, text = "Username", font = ("Arial", 18), fg_color = "#3095AE", text_color = "black", padx = 5, pady = 6,corner_radius=5)
+    username_input = ctk.CTkEntry(center_frame, textvariable=user_var, font=("Arial", 15), fg_color="#BEE9E8", text_color="black", width=400, corner_radius=5)
+    username_label.grid(row=1, column=0, sticky="e", padx=5, pady=10, ipady = 4)
+    username_input.grid(row=1, column=1, sticky="w", padx=20, pady=10, ipady=5)
 
-canvas = ctk.CTkCanvas(center_frame, width = 500, height = 3, bg = "#1B4965", highlightthickness= 0)
-canvas.grid(row = 5, column = 0, columnspan = 2, pady = 20)
+    # password entry
+    password_label = ctk.CTkLabel(center_frame, text="Password ", font=("Arial", 18), fg_color="#3095AE",text_color="black", padx=5, pady=6, corner_radius=5)
+    password_input = ctk.CTkEntry(center_frame, textvariable=password_var, font=("Arial", 15), fg_color="#BEE9E8", text_color = "black", width=400, show="*", corner_radius=5)
+    password_label.grid(row=2, column=0, sticky="e", padx=5, pady=10,ipady = 4)
+    password_input.grid(row=2, column=1, sticky="w", padx=20, pady=10, ipady=5)
 
-signup_button = ctk.CTkButton(center_frame, text="Create an account", font=("Arial", 20), fg_color="#3095AE", hover_color="#246690", text_color="black", width=600, corner_radius=15, command=create_new_account)
-signup_button.grid(row=6, column=0, columnspan=2, pady=5, padx=5, ipady = 6)
+    # forgot password button
+    forgot_button = ctk.CTkButton(center_frame, text="Forgot Password?", font=("Arial", 13), fg_color="#3095AE", hover_color="#246690", text_color="black", width=130, corner_radius=5, command=forgot_password)
+    forgot_button.grid(row=3, column=1, columnspan=2, padx=50, sticky="e")
+
+    # login button #1B4965 and then 246690
+    login_button = ctk.CTkButton(center_frame, text="Login", font=("Arial", 18), fg_color="#3095AE", hover_color="#246690", text_color="black", width=200, corner_radius=5, command=on_login)
+    login_button.grid(row=4, column=0, columnspan=2, pady=5, padx=50, ipady = 5)
+
+    canvas = ctk.CTkCanvas(center_frame, width = 500, height = 3, bg = "#1B4965", highlightthickness= 0)
+    canvas.grid(row = 5, column = 0, columnspan = 2, pady = 20)
+
+    signup_button = ctk.CTkButton(center_frame, text="Create an account", font=("Arial", 20), fg_color="#3095AE", hover_color="#246690", text_color="black", width=600, corner_radius=15, command=create_new_account)
+    signup_button.grid(row=6, column=0, columnspan=2, pady=5, padx=5, ipady = 6)
+
+show_login_page()
 
 root.mainloop()
--- venv/main.py
+++ venv/main.py
@@ -1,51 +1,15 @@
-#this is a financial management app programmed by Nikhila and Sharvika.
+# this is a financial management app programmed by Nikhila and Sharvika.
 
-#importing all the necessary libraries
-#importing matplotlib --- This would be used for graphing data (to review b/c error)
-#import matplotlib.pyplot as plt
+# importing all the necessary libraries
+# importing matplotlib --- This would be used for graphing data (to review b/c error)
+# import matplotlib.pyplot as plt
 
-#importing tkinter --- it would be used for creating a GUI
-import tkinter as tk
-from tkinter import *
-from tkinter import ttk #apparently this is useful to style the tk widgets like how css is used to style
+import customtkinter as ctk
+import tkinter as tk  # for StringVar + messagebox + lines
+from tkinter import messagebox
+import root_window
+from root_window import *
 
-
-#creating another window to store the SQL commands
-def database_display_window():
-    database_window = tk.Toplevel(root)
-    database_window.title("Your transaction history")
-    database_window.geometry("800x600")
-
-
-    close_button = tk.Button(database_window, text="Close", command=database_window.destroy)
-    close_button.grid(row=0, column=0, pady=10)
-
-#root is root window which will contain *everything*
-#Setting up the root window
-root = tk.Tk()
-root.title("Home Page")
-root.geometry("800x600")
-#root.attributes('-fullscreen', True) #causing the problem w/ the desktop
-
-
-def onceClicked():
-    label = Label(root, text = "i clicked the thing")
-    label.grid(row = 3, column = 0)
-
-
-#label examples 4 reference
-tl1 = Label(root, text= "welcome")
-tl1.grid(row = 0, column = 0)
-tl2 = Label(root, text= "test label 2")
-tl2.grid(row = 1, column = 0)
-
-#button examples 4 reference
-#command is basically what action happens once the button is clicked. you can make a function for that and you can use that function as a command. this timee it was teh onceClicked function
-tb1 = Button(root, text = "test button1", padx = 20, pady = 50, command = onceClicked)
-tb1.grid(row = 2, column = 0)
-
-
-open_button = tk.Button(root, text="Open Database", command=database_display_window)
-open_button.grid(row=4, column=0, pady=20)
+root_window.root_init()
 
 root.mainloop()
