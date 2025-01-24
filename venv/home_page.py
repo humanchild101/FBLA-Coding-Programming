09d7bd0638e7844536cb47738b9aed647e9dc394
Nikhila Mannepalli <nikhilamadhavi@gmail.com>
2025-01-24 10:09:43 -0500

    so apparently files didn't add before, but i figured out how to add them through git

---
+++ venv/home_page.py
@@ -0,0 +1,14 @@
+import customtkinter as ctk
+import tkinter as tk  # for StringVar + messagebox + lines
+from tkinter import messagebox
+import login
+from login import *
+import root_window
+from root_window import *
+
+root = root_window.root_init()
+
+def show_home_page():
+    return
+
+
