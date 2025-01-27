bfef8dba59f754542497928f8733ca2855d19080
Sharvikaks <sharvika.ks@gmail.com>
2025-01-26 20:18:37 -0500

    Create account database completed

--- scripts/db_connect.py
+++ scripts/db_connect.py
@@ -1,9 +1,11 @@
-#logging is a module that is used to log the messages to the console or file.
+#logging is a module that is used to log the messages to the console or file 
+#Initially imported logging -- not used so far
 import logging
-import psycopg2
+
+import psycopg2 #will be used for connecting to db
 
 # Connection parameters to the database
-
+# Contains details of the database login -- will be used for connecting
 db_params = {
     "host": "localhost",
     "database": "postgres",
@@ -15,8 +17,7 @@ db_params = {
 
 def get_connection():
     try:
-        # Connect to the database
-        conn = psycopg2.connect(**db_params)
+        conn = psycopg2.connect(**db_params) #Connect to the database
         return conn
     except (Exception, psycopg2.Error) as error:
         print("Error connecting to database:", error)
@@ -25,22 +26,13 @@ def get_connection():
         
 def execute_query(query):
     try:
-        # Connect to the database
-        conn = get_connection()
-
-        # Create a cursor object
-        cursor = conn.cursor()
-
-        # Execute a query
-        cursor.execute(query)
-
-        # Fetch the result
-        result = cursor.fetchone()
-
-        # return the result
+        conn = get_connection() # Connect to the database
+        cursor = conn.cursor() #Cursor is used to navigate through the db
+        cursor.execute(query) # For executing the SQL query statements
+        result = cursor.fetchone() # To get the values from db and return those
         return result
 
-    except (Exception, psycopg2.Error) as error:
+    except (Exception, psycopg2.Error) as error: 
         print("Error connecting to database:", error)
         if cursor:
             cursor.close()
@@ -53,4 +45,25 @@ def execute_query(query):
             cursor.close()
         if conn:
             conn.close()
+
+
+
+def insert_values(query):
+    try:
+        conn = get_connection() # Connect to the database
+        cursor = conn.cursor() #Cursor is used to navigate through the db
+        print(query)
+        cursor.execute(query) # For executing the SQL query statements
+        conn.commit()
+
+    except (Exception, psycopg2.Error) as error: 
+        raise Exception(error)
+
+    finally:
+        # Close the cursor and connection
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
 
