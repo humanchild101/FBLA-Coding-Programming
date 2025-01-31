26fcb27a45ed272b1e2b7ff75c5b5a0b0095c78a
Nikhila Mannepalli <nikhilamadhavi@gmail.com>
2025-01-30 20:37:13 -0500

    commit

--- venv/inputs.py
+++ venv/inputs.py
@@ -1,8 +1,9 @@
+#SSHARVIKAAAAAAAAAAAAAA
+#need i say any more?
 import customtkinter as ctk
 import tkinter as tk  # for StringVar + messagebox + lines
 from tkinter import messagebox
-import tkcalendar
-from tkcalendar import Calendar
+from tkinter import ttk
 import root_window
 from root_window import *
 import datetime
@@ -28,6 +29,29 @@ face4 = ctk.CTkImage(light_image=Image.open("/Users/nikhila/FBLA-Coding-Programm
 face5 = ctk.CTkImage(light_image=Image.open("/Users/nikhila/FBLA-Coding-Programming/faces/very_sad.png"), size=(70,70))
 
 status_area = ctk.CTkLabel(user_label, text="", image=face1, fg_color="#1B4965", font=("Arial", 30, "bold"), text_color="#BEE9E8", padx=20, height=70, width = 80)
+
+# Manual Budget Update
+monthly_budget_frame = ctk.CTkFrame(inputs_page, fg_color="#62B6CB", corner_radius=20, width=640, height=100)
+monthly_budget_frame.grid(row=1, column=0, padx=15, pady=15, sticky="e")
+
+# Manual Balance Updates
+manual_balance_update_frame = ctk.CTkFrame(inputs_page, fg_color="#62B6CB", corner_radius=20, width=480, height=100)
+manual_balance_update_frame.grid(row=1, column=1, padx=15, pady=15, sticky="w")
+
+tab_view = ctk.CTkTabview(inputs_page, fg_color="#1B4965", corner_radius=20, width=1100, height=600)
+tab_view.place(relx=0.026, rely=0.2)
+
+transactions = tab_view.add("Transactions")
+deposits = tab_view.add("Deposits")
+income_sources = tab_view.add("Income Sources")
+
+#STRING VARS
+amount_var = tk.StringVar(value="0.00")
+purpose_var = tk.StringVar(value="Choose")  # Default value set to 'Need'
+date_options = [(datetime.date.today() - datetime.timedelta(days=i)).strftime("%m-%d-%Y") for i in range(500)]
+date_var = ctk.StringVar(value=date_options[0])  # Default to today's date
+need_var = tk.StringVar(value="Need")  # Default value set to 'Need'
+
 
 def hide():
     inputs_page.forget()
@@ -59,9 +83,6 @@ def show():
 
     status_area.grid(row = 0, column = 1, sticky = "w", padx = 2)
 
-
-    #ADD SMILY FACE HERE ^
-
     # iputs tab name
     inputs_label = ctk.CTkLabel(top_nav, fg_color="#1B4965", padx = 20, height = 80, width =200, text = "")
     inputs_label.grid(row=0, column = 1, padx = 12)
@@ -82,22 +103,6 @@ def show():
     logoutbutton.grid(row=0, column=2, padx=15, pady=10, sticky="e")
 
 
-    # Manual Budget Update
-    monthly_budget_frame = ctk.CTkFrame(inputs_page, fg_color="#62B6CB", corner_radius=20, width=640, height=100)
-    monthly_budget_frame.grid(row=1, column=0, padx=15, pady=15, sticky="e")
-
-    # Manual Balance Updates
-    manual_balance_update_frame = ctk.CTkFrame(inputs_page, fg_color="#62B6CB", corner_radius=20, width=480, height=100)
-    manual_balance_update_frame.grid(row=1, column=1, padx=15, pady=15, sticky="w")
-
-    tab_view = ctk.CTkTabview(inputs_page, fg_color = "#1B4965", corner_radius= 20, width = 1100, height =600)
-    tab_view.place(relx = 0.026, rely = 0.2)
-
-    transactions = tab_view.add("Transactions")
-    deposits = tab_view.add("Deposits")
-    income_sources = tab_view.add("Income Sources")
-
-    amount_var = tk.StringVar(value = "0.00")
     amount_label = ctk.CTkLabel(transactions, fg_color="#62B6CB", corner_radius=5, width=60, height=40, text_color="black", font = ("Arial", 20, "bold"), text = "Amount: ")
     amount_label.place(relx = 0.001, rely = 0.01)
     amount_entry = ctk.CTkEntry(transactions, width=100, height = 40, textvariable = amount_var, font=("Arial", 12))
@@ -105,7 +110,6 @@ def show():
 
     purpose_label = ctk.CTkLabel(transactions, fg_color="#62B6CB", corner_radius=5, width=60, height=40, text_color="black", font = ("Arial", 20, "bold"), text = "Purpose of Transaction: ")
     purpose_label.place(relx = 0.001, rely = 0.13)
-    purpose_var = tk.StringVar(value="Choose")  # Default value set to 'Need'
     purpose_options = ["Bills/Rent", "Necessities", "Transportation", "Healthcare", "Education", "Donations", "Entertainment", "Other"]
     purpose_dropdown = ctk.CTkOptionMenu(transactions, variable=purpose_var, values=purpose_options, font=("Arial", 15), height = 40, text_color= "black",fg_color= "#BEE9E8", button_color= "#BEE9E8", button_hover_color="#A0D8D3")
     purpose_dropdown.place(relx=0.25, rely=0.13)
@@ -115,19 +119,40 @@ def show():
     date_label.place(relx=0.001, rely=0.28, anchor="w")
 
 
-    calendar = Calendar(transactions, selectmode="day", date_pattern="yyyy-mm-dd", font=("Arial", 12), width=20, background="lightblue", foreground="black", borderwidth=1)
-    calendar.place(relx=0.25, rely=0.25, anchor="w")
-
+    date_dropdown = ctk.CTkOptionMenu(transactions, variable=date_var, values=date_options, font=("Arial", 15), height=40, text_color= "black",fg_color= "#BEE9E8", button_color= "#BEE9E8", button_hover_color="#A0D8D3")
+    date_dropdown.place(relx = 0.083, rely = 0.24)
 
     # need/want radio buttons
-    need_want = ctk.CTkLabel(transactions, text="", fg_color="#62B6CB", corner_radius=5, height = 80, width = 500)
-    need_want.place(relx=0.47, rely=0.09, anchor="w")
+    need_want = ctk.CTkLabel(transactions, text="", fg_color="#62B6CB", corner_radius=5, height = 80, width = 400)
+    need_want.place(relx=0.47, rely=0.01, anchor="nw")
 
-    need_var = tk.StringVar(value="Need")  # Default value set to 'Need'
-    need_radio = ctk.CTkRadioButton(need_want, text="Need", variable=need_var, value="Need", font=("Arial", 20, "bold"), width=60, text_color="black", fg_color="#BEE9E8",hover_color="#1B4965")
+    need_radio = ctk.CTkRadioButton(need_want, text="Need (N)", variable=need_var, value="Need", font=("Arial", 20, "bold"), width=40, text_color="black", fg_color="#BEE9E8",hover_color="#1B4965")
     need_radio.place(relx=0.1, rely=0.5, anchor="w")
-    want_radio = ctk.CTkRadioButton(need_want, text="Want", variable=need_var, value="Want", font=("Arial", 20, "bold"), width=60, text_color="black", fg_color="#BEE9E8",hover_color="#1B4965")
+    want_radio = ctk.CTkRadioButton(need_want, text="Want (W)", variable=need_var, value="Want", font=("Arial", 20, "bold"), width=40, text_color="black", fg_color="#BEE9E8",hover_color="#1B4965")
     want_radio.place(relx=0.5, rely=0.5, anchor="w")
+
+    #SHARVIKAAAAAAAAAAAA
+    input_button = ctk.CTkButton(transactions, text="Input Transaction", font=("Arial", 20, "bold"), fg_color="#9AC0BF", hover_color="#467F8D", height = 40, text_color="black", width=300, corner_radius=10, command=None)
+    input_button.place(relx = 0.5, rely = 0.24)
+
+    transactions_table = ttk.Treeview(transactions, columns = ("No.", "Transaction", "N/W", "Amount", "Date", "Del"), show = "headings")
+
+    transactions_table.heading("No.", text="No.")
+    transactions_table.heading("Transaction", text="Transaction")
+    transactions_table.heading("N/W", text="N/W")
+    transactions_table.heading("Amount", text="Amount")
+    transactions_table.heading("Date", text="Date")
+    transactions_table.heading("Del", text="Del")
+
+    transactions_table.column("No.", width=50)
+    transactions_table.column("Transaction", width=150)
+    transactions_table.column("N/W", width=80)
+    transactions_table.column("Amount", width=100)
+    transactions_table.column("Date", width=100)
+    transactions_table.column("Del", width=80)
+
+
+
 
 show()
 root.mainloop()
